cb666d037736c7dba61d70d100621ca89ab67a23
Pontus Forsman <forsmanpontus@gmail.com>
2025-04-22 21:47:39 +0200

    added comment where we left off

--- blockchain.py
+++ blockchain.py
@@ -59,3 +59,5 @@ class Blockchain(object):
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+    
+    # PART 2 AND ONWARDS
